2723bfcc935e3b8a95e9948b92a83ed975f4f2fe
Blueeyesjt1 <jadenwilliamsjtw@gmail.com>
2020-10-17 10:00:05 -0400

    Issue 2 updates version 2

---
+++ src/main/java/AudioPlayer.java
@@ -0,0 +1,36 @@
+public class AudioPlayer extends Product implements MultimediaControl {
+
+  String supportedAudioFormats;
+  String supportedPlaylistFormats;
+
+  public AudioPlayer(String name, String manufacturer, String supportedAudioFormats, String supportedPlaylistFormats) {
+    super(name, manufacturer, ItemType.AUDIO);
+    this.supportedAudioFormats = supportedAudioFormats;
+    this.supportedPlaylistFormats = supportedPlaylistFormats;
+  }
+
+  public String toString() {
+    return "Name: " + name +
+        "\n Manufacturer: " + manufacturer +
+        "\n Type: " + type +
+        "\n Support Audio Formats: " + supportedAudioFormats +
+        "\n Supported Playlist Formats: " + supportedPlaylistFormats;
+  }
+
+  public void Play() {
+    System.out.println("Playing");
+  }
+
+  public void Stop() {
+    System.out.println("Stopping");
+  }
+
+  public void Previous() {
+    System.out.println("Previous");
+  }
+
+  public void Next() {
+    System.out.println("Next");
+  }
+
+}
